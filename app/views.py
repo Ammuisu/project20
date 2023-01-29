9789e67eca261dcf179c16b21d6f09a71c86154d
Ammuisu <tjyothi.geetha@gmail.com>
2023-01-29 15:13:33 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def jyothi(request):
+    return HttpResponse('this is an user defined base_url')
